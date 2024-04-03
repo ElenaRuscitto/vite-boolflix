4dad3d76c51fa2726851afd9a4d908025e41c2e4
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-04-03 15:30:10 +0200

    [add] header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,14 +13,36 @@ export default {
 
 
 <template>
-  <div>
-    header
+  <header>
+  <div class="container d-flex justify-content-between">
+    <div>
+      <h1>BOOLFLIX</h1>
+    </div>
+    <div class="d-flex">
+      <input type="text" class="form-control w-100 ">
+      <button class="btn btn-danger">Cerca</button>
+    </div>
   </div>
+  </header>
 </template>
 
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/general' as *;
 @use '../assets/scss/partials/variables' as *;
+
+header {
+  background-color: black;
+  color: red;
+  line-height: 70px;
+  input {
+    margin: 10px;
+  }
+  .btn {
+    height: 40px;
+    margin-top: 10px ;
+  }
+}
+
 
 </style>
